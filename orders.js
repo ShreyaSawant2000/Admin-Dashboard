0084ed94e4edde7cf803609425f68865479d0804
ShreyaSawant2000 <primeshreya2000@gmail.com>
2022-01-23 19:25:20 +0530

    initial commit

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders =[
+{
+    productName: 'foldable Mini Drone',
+    productNumber:'86662',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+{
+    productName: 'Lavender Mini Drone',
+    productNumber:'82662',
+    paymentStatus:'Refunded',
+    shipping:'Declined'
+},
+{
+    productName: 'Ruko F11pro drone',
+    productNumber:'44362',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+{
+    productName: 'Drone with Camera Drone',
+    productNumber:'86662',
+    paymentStatus:'Due',
+    shipping:'Delivered'
+},
+{
+    productName: 'foldable Mini Drone',
+    productNumber:'86662',
+    paymentStatus:'Paid',
+    shipping:'Delivered'
+},
+]
